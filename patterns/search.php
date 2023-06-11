997fa645b9e3f7ac5c6769f6eb51b5998232f542
Pea <pea@misfist.com>
2023-06-10 21:25:54 -0400

    Update search component

--- patterns/search.php
+++ patterns/search.php
@@ -6,21 +6,75 @@
  * Keywords: Search facets
  * Block Types: core/buttons
  */
-
-global $wp_query;
-// var_dump( get_queried_object() );
 ?>
 
-<!-- wp:query-title {"type":"search","align":"wide"} /-->
-<!-- wp:tag-cloud {"taxonomy":"category"} /-->
-<!-- wp:tag-cloud {"taxonomy":"product_type"} /-->
-<!-- wp:tag-cloud {"taxonomy":"post_tag"} /-->
-<!-- wp:tag-cloud {"taxonomy":"entity"} /-->
-<!-- wp:tag-cloud {"taxonomy":"person"} /-->
-<!-- wp:elasticpress/facet {"facet":"category","orderby":"name","order":"asc","className":"facet__programs"} /-->
-<!-- wp:elasticpress/facet {"facet":"product_type","orderby":"name","order":"asc","className":"facet__product"} /-->
-<!-- wp:elasticpress/facet {"facet":"post_tag","orderby":"name","order":"asc","className":"facet__topic"} /-->
-<!-- wp:elasticpress/facet {"facet":"person","orderby":"name","order":"asc","className":"facet__person"} /-->
-<!-- wp:elasticpress/facet {"facet":"author","orderby":"name","order":"asc","className":"facet__author"} /-->
-<!-- wp:elasticpress/facet {"facet":"entity","orderby":"name","order":"asc","className":"facet__entity"} /-->
-<!-- wp:search {"label":"Search","align":"wide","buttonText":"Search","buttonUseIcon":true} /-->
+
+<!-- wp:group {"align":"wide","className":"facet programs","layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide facet programs" id="program-areas">
+    <!-- wp:heading {"level":3,"align":"wide"} -->
+    <h3 class="wp-block-heading alignwide">Program Areas</h3>
+    <!-- /wp:heading -->
+
+    <!-- wp:elasticpress/facet {"facet":"category","orderby":"name","order":"asc","className":"facet__programs alignwide"} /-->
+</div>
+<!-- /wp:group -->
+
+<!-- wp:group {"align":"wide","className":"facet topics","layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide facet topics" id="topics">
+    <!-- wp:heading {"level":3,"align":"wide"} -->
+    <h3 class="wp-block-heading alignwide">Topics</h3>
+    <!-- /wp:heading -->
+
+    <!-- wp:elasticpress/facet {"facet":"post_tag","orderby":"name","order":"asc","className":"facet__topics alignwide"} /-->
+</div>
+<!-- /wp:group -->
+
+<!-- wp:group {"align":"wide","className":"facet people","layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide facet people" id="people">
+    <!-- wp:heading {"level":3,"align":"wide"} -->
+    <h3 class="wp-block-heading alignwide">People</h3>
+    <!-- /wp:heading -->
+
+    <!-- wp:elasticpress/facet {"facet":"person","orderby":"name","order":"asc","className":"facet__people alignwide"} /-->
+</div>
+<!-- /wp:group -->
+
+<!-- wp:group {"align":"wide","className":"facet product-types","layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide facet product-types" id="product-types">
+    <!-- wp:heading {"level":3,"align":"wide"} -->
+    <h3 class="wp-block-heading alignwide">Product Types</h3>
+    <!-- /wp:heading -->
+
+    <!-- wp:elasticpress/facet {"facet":"product_type","orderby":"name","order":"asc","className":"facet__product-types alignwide"} /-->
+</div>
+<!-- /wp:group -->
+
+<!-- wp:group {"align":"wide","className":"facet entities","layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide facet entities" id="entitities">
+    <!-- wp:heading {"level":3,"align":"wide"} -->
+    <h3 class="wp-block-heading alignwide">Entities</h3>
+    <!-- /wp:heading -->
+
+    <!-- wp:elasticpress/facet {"facet":"entity","orderby":"name","order":"asc","className":"facet__entities alignwide"} /-->
+</div>
+<!-- /wp:group -->
+
+<!-- wp:group {"align":"wide","className":"facet regions","layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide facet regions" id="regions">
+    <!-- wp:heading {"level":3,"align":"wide"} -->
+    <h3 class="wp-block-heading alignwide">Regions</h3>
+    <!-- /wp:heading -->
+
+    <!-- wp:elasticpress/facet {"facet":"region","orderby":"name","order":"asc","className":"facet__regiions alignwide"} /-->
+</div>
+<!-- /wp:group -->
+
+<!-- wp:group {"align":"wide","className":"facet search","layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide facet search" id="search">
+    <!-- wp:heading {"level":3,"align":"wide"} -->
+    <h3 class="wp-block-heading alignwide">Keyword</h3>
+    <!-- /wp:heading -->
+
+    <!-- wp:search {"label":"Search","showLabel":false,"buttonText":"Search","buttonUseIcon":true,"align":"wide","className":"alignwide"} /-->
+</div>
+<!-- /wp:group -->
